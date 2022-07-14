d6ccc13c9eacb116696347e350d2586f46b63ee9
Corrigan Baker <corriganbaker@gmail.com>
2022-07-14 18:41:21 -0400

    new 5kyu challenge addded

---
+++ 5kyu/first-non-repeating-character.js
@@ -0,0 +1,40 @@
+// Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.
+
+// For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+
+// As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+
+// If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.
+
+// function firstNonRepeatingLetter(s) {
+//   return (
+//     s[
+//       s
+//         .toLowerCase()
+//         .split("")
+//         .findIndex(
+//           letter =>
+//             s
+//               .toLowerCase()
+//               .split("")
+//               .filter(l => l === letter).length === 1
+//         )
+//     ] || ""
+//   );;
+// }
+
+// closer to my orginal try, but kept failing
+// function firstNonRepeatingLetter(s) {
+//   let str = s.toLowerCase();
+//   for(let i = 0; i < str.length; i++) {
+//     if(str.indexOf(str[i]) === str.lastIndexOf(str[i])) {
+//       return s[i];
+//     }
+//   }
+//   return "";
+// }
+
+// shorter solution i found
+function firstNonRepeatingLetter(s){
+  return s.split('').find(e => s.match(new RegExp(`${e}`, 'gi')).length === 1) || ''
+}

2a970b037a002b285aadcd1e4d4a7825c2e671f7
Corrigan Baker <corriganbaker@gmail.com>
2022-09-26 11:36:55 -0400

    added startswith method to new 8kyu

--- 8kyu/are-you-playing-banjo.js
+++ 8kyu/are-you-playing-banjo.js
@@ -3,15 +3,22 @@
 
 // The function takes a name as its only argument, and returns one of the following strings:
 
-// name + " plays banjo" 
+// name + " plays banjo"
 // name + " does not play banjo"
 // Names given are always valid strings.
 
-function areYouPlayingBanjo(name) {
-  if (name.toLowerCase().charAt(0) == "r") {
-    return name + " plays banjo";
-  } else {
-    return name + " does not play banjo";
-  }
-  return name;
-}
+// first tryyyy
+// function areYouPlayingBanjo(name) {
+//   if (name.toLowerCase().charAt(0) == "r") {
+//     return name + " plays banjo";
+//   } else {
+//     return name + " does not play banjo";
+//   }
+//   return name;
+// }
+
+// using startWith method
+const areYouPlayingBanjo = (name) => {
+  const plays = name.toLowerCase().startsWith("r") ? "plays" : "does not play";
+  return `${name} ${plays} banjo`;
+};

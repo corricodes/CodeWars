1bef5eba9af12f34d929f32e607d26c88fde6368
Corrigan Baker <corriganbaker@gmail.com>
2022-09-26 17:05:09 -0400

    added another 8kyu w/ extra solutions

---
+++ 8kyu/reversed-strings.js
@@ -0,0 +1,19 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+
+// // first tryyy
+// function solution(str) {
+//   return str === "" ? "" : solution(str.substr(1)) + str.charAt(0);
+// }
+// solution("kitty");
+
+// // simpler
+// function solution(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// using spread operator
+const solution = (s) => [...s].reverse().join("");
